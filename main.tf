9a1679110032a2fc608946f29fde66cc9454195f
DPS34 <163555413+DPS34@users.noreply.github.com>
2024-07-11 15:32:52 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,15 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
   cluster_version = "1.21"
-  subnets         = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
+  subnet_ids      = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
   vpc_id          = aws_vpc.eks_vpc.id
 
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_type = "t3.medium"
-    }
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+    instance_type = "t3.medium"
+    desired_capacity = 2
+    min_size = 1
+    max_size = 3
   }
 
   tags = {
