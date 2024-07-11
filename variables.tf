e533ed3cd98231be4d99015dceb0e4380e7a03c6
DPS34 <163555413+DPS34@users.noreply.github.com>
2024-07-11 15:33:56 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,10 +15,12 @@ variable "vpc_cidr" {
 
 variable "public_subnets" {
   description = "Public subnet CIDRs"
+  type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
   description = "Private subnet CIDRs"
+  type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
