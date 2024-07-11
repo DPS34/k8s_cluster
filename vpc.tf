c2927bab42ec166f06af5a28e269da6006e35fad
DPS34 <163555413+DPS34@users.noreply.github.com>
2024-07-11 12:13:26 -0300

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+}
+
+data "aws_availability_zones" "available" {}
